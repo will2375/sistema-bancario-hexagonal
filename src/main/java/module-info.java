1df755f6eb331f238bfa2fcc79543f901fef0af6
will2375 <wil.liam_benicio@hotmail.com>
2023-01-06 17:02:42 -0300

    implementação de portas e usecase

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,14 +7,20 @@ module conta.sistema {
 
 
     // expondo porta de entrada (driver)
-
+    exports conta.sistema.casouso.porta;
     exports conta.sistema.casouso.imp;
 
     // expondo sistema negocio
-
+    exports conta.sistema.dominio.modelo;
     exports conta.sistema.dominio.servico;
 
     // expondo adaptadores de saídas (driven)
-
+    exports conta.sistema.porta;
     exports conta.adaptador;
+
+    //abre reflexão spring
+    opens conta.sistema.casouso.porta;
+    opens conta.sistema.casouso.imp;
+    opens conta.sistema.dominio.servico;
+    opens conta.adaptador;
 }
